f5ce53d6e34d4227379ff60ebae6ef55b844e2d7
pushu <HP@pushu.push>
2025-04-17 12:17:14 +0530

    Fixed View Resume HTML path

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -5,7 +5,7 @@ function Resume() {
     <div className="section">
       <h2>My Resume</h2>
       <p>You can download my resume below.</p>
-      <a href="/resume.html" target="_blank" rel="noopener noreferrer">View My-Resume</a> 
+      <a href="/MY-PORTFOLIO-WEBSITE/resume.html" target="_blank" rel="noopener noreferrer">View My-Resume</a> 
       <br/>
       <a href="/my-Resume.md" download> Download My-Resume(README)</a>
     </div>
