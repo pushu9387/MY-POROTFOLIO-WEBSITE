e290972459352f3d46e899f08764a3b7c653aae6
pushu <HP@pushu.push>
2025-04-17 10:50:34 +0530

    Build updated with new resume feature

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -5,7 +5,8 @@ function Resume() {
     <div className="section">
       <h2>My Resume</h2>
       <p>You can download my resume below.</p>
-      <a href="/resume.pdf" download className="resume-button">Download My Resume</a>
+      <a href="/resume.html" target='_blank' rel='noopener noreferrer'>View My Resume</a> 
+      <a href="/resume.html" download> Download Resume</a>
     </div>
   );
 }
